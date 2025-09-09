991cf963f8680d0c2d93cc6768403d8ad1b88d06
Anibal509 <111336421+Anibal509@users.noreply.github.com>
2025-09-08 19:30:42 -0500

    Add files via upload

---
+++ PARCIALES/PARCIAL_1/gestionar_membresia.php
@@ -0,0 +1,20 @@
+<?php
+
+ $Membresia=[
+    "Basico"=>85,
+   "premium" => 140,
+   "vip" => 200,
+   "familiar" =>250,
+   "empresarial"=> 300,
+ ];
+    
+ $miembros=[
+    "Carla Perez" => ["tipo"=> "premiun", "antiguedad"=> 20 ],
+    "Juan Garcia" => ["tipo"=> "premiun", "antiguedad"=> 3],
+    "Ana Lopez" => ["tipo"=> "premiun", "antiguedad"=> 50 ],
+    "Sofia Hurtado" => ["tipo"=> "premiun", "antiguedad"=> 6 ],
+    "ALvaro Dominguez" => ["tipo"=> "premiun", "antiguedad"=> 90 ],
+    "Maria Alveo" => ["tipo"=> "premiun", "antiguedad"=> 4 ],
+    "Ceciclia Rodriguez" => ["tipo"=> "premiun", "antiguedad"=> 12 ],
+ ]
+?>
